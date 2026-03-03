2d5c74c29de7cc47bc2ac5ddae4d332329f2a2f2
chesleylaughlin <chesleylaughlin0@gmail.com>
2026-03-03 11:07:18 +0530

    Refactor OOPSBANNERAPP class and add output

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,6 +1,13 @@
-public class OOPSBANNERAPP{
-    public static void main(String[] args) {
-        System.out.println("initializes");
-    }
+public class OOPSBannerApp {
 
+    public static void main(String[] args) {
+
+        System.out.println("   ***   " + "   " + "   ***   " + "   " + " ******  " + "   " + "  *****  ");
+        System.out.println(" **   ** " + "   " + " **   ** " + "   " + " **    **" + "   " + " **   ** ");
+        System.out.println("**     **" + "   " + "**     **" + "   " + " **    **" + "   " + " **      ");
+        System.out.println("**     **" + "   " + "**     **" + "   " + " ******  " + "   " + "  *****  ");
+        System.out.println("**     **" + "   " + "**     **" + "   " + " **      " + "   " + "      ** ");
+        System.out.println(" **   ** " + "   " + " **   ** " + "   " + " **      " + "   " + " **   ** ");
+        System.out.println("   ***   " + "   " + "   ***   " + "   " + " **      " + "   " + "  *****  ");
+    }
 }
